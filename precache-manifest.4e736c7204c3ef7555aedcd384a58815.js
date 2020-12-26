84f426e6f35c5769cc4bb80a5239ddf5b46445a1
Kateechkin <skulkina.web@gmail.com>
2020-12-27 00:45:00 +0700

    admin-xfence

---
+++ precache-manifest.4e736c7204c3ef7555aedcd384a58815.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e861da42fffa4475e482",
+    "url": "/css/app.52124d7e.css"
+  },
+  {
+    "revision": "606019b0558e70b2bcb26bee438bc0e3",
+    "url": "/img/cart.606019b0.png"
+  },
+  {
+    "revision": "2e00c4308cc8e05b9670473d5a3f4bed",
+    "url": "/img/logo.2e00c430.png"
+  },
+  {
+    "revision": "fd033c148709b43f8d976b4c707af199",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e861da42fffa4475e482",
+    "url": "/js/app.91d0a64a.js"
+  },
+  {
+    "revision": "adfa49285f12c2367b3e",
+    "url": "/js/chunk-vendors.dc27a3cd.js"
+  },
+  {
+    "revision": "bae6c57f694db75b49be8c9b431ee32c",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
